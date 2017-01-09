19e39dfb7ac4ac8f1db9efe3bd9e190b35ca7c8e
Marcin Flis <marcin.flis.95@gmail.com>
2017-01-09 17:00:20 +0100

    stopping does not work

--- src/main/scala-2.12/pl/agh/edu/dehaser/Messages.scala
+++ src/main/scala-2.12/pl/agh/edu/dehaser/Messages.scala
@@ -3,13 +3,13 @@ package pl.agh.edu.dehaser
 import scala.collection.immutable.NumericRange
 
 
-sealed trait Messages
+sealed trait CheckResponse
 
-case class DehashIt(hash: String, algo: String) extends Messages
+case class DehashIt(hash: String, algo: String)
 
 case class Check(range: NumericRange[BigInt], hash: String, algo: String)
 
 // TODO: send original hash and algo or not?
-case class FoundIt(crackedPass: String)
+case class FoundIt(crackedPass: String) extends CheckResponse
 
-case class RangeChecked(range: NumericRange[BigInt])
+case class RangeChecked(range: NumericRange[BigInt]) extends CheckResponse
